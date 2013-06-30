3b09aeca7aa79a1bad2db986245fe046fb1406bf
Mayank Gupta <mayankgupta2005@gmail.com>
2013-06-30 23:34:05 +0530

    MDL-40391 mod_scorm: scorm_next() rewrite in YUI3

--- mod/scorm/module.js
+++ mod/scorm/module.js
@@ -400,12 +400,18 @@ M.mod_scorm.init = function(Y, hide_nav, hide_toc, toc_title, window_name, launc
 
         var scorm_next = function(node, update_launch_sco) {
             if (node === false) {
-                return scorm_tree_node.getRoot().children[0];
+                return scorm_tree_node.rootNode.children[0];
             }
             if (node.children.length && typeof scoes_nav[launch_sco].nextscoid != 'undefined') {
                 var node = node.children[0];
                 var nextscoid = scoes_nav[launch_sco].nextscoid;
-                node.title = scoes_nav[nextscoid].url;
+                if (node.title != scoes_nav[nextscoid].url) {
+                    node = scorm_tree_node.getNodeByAttribute('title', scoes_nav[nextscoid].url);
+                    if (node === null) {
+                        node = scorm_tree_node.rootNode.children[0];
+                        node.title = scoes_nav[nextscoid].url;
+                    }
+                }
                 if (update_launch_sco) {
                     launch_sco = nextscoid;
                 }
