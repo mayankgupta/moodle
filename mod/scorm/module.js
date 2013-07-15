3dbb17da078fd852d63d48f4ec9073be11212e87
Mayank Gupta <mayankgupta2005@gmail.com>
2013-07-16 00:05:11 +0530

    MDL 40636 mod_scorm: update scorm_activate_item(), yui3 grids

--- mod/scorm/module.js
+++ mod/scorm/module.js
@@ -169,22 +169,45 @@ M.mod_scorm.init = function(Y, hide_nav, hide_toc, toc_title, window_name, launc
                 document.getElementById('external-scormapi').src = api_url;
             }
 
-            var content = new Y.YUI2.util.Element('scorm_content');
-            var obj = document.createElement('iframe');
-            obj.setAttribute('id', 'scorm_object');
-            obj.setAttribute('type', 'text/html');
-            if (!window_name && node.title != null) {
-                obj.setAttribute('src', url_prefix + node.title);
-            }
-            if (window_name) {
-                var mine = window.open('','','width=1,height=1,left=0,top=0,scrollbars=no');
-                if(! mine) {
-                    alert(M.str.scorm.popupsblocked);
+            var content = new Y.one('#scorm_content');
+            try {
+                // first try IE way - it can not set name attribute later
+                // and also it has some restrictions on DOM access from object tag
+                if (window_name || node.title == null) {
+                    var obj = document.createElement('<iframe id="scorm_object" src="">');
+                    if (window_name) {
+                        var mine = window.open('','','width=1,height=1,left=0,top=0,scrollbars=no');
+                        if(! mine) {
+                             alert(M.str.scorm.popupsblocked);
+                        }
+                        mine.close()
+                    }
+                }
+                else {
+                    var obj = document.createElement('<iframe id="scorm_object" src="'+url_prefix + node.title+'">');
+                }
+                // fudge IE7 to redraw the screen
+                if (Y.YUI2.env.ua.ie > 5 && Y.YUI2.env.ua.ie < 8) {
+                    obj.attachEvent("onload", scorm_resize_parent);
+                }
+            } catch (e) {
+                var obj = document.createElement('object');
+                obj.setAttribute('id', 'scorm_object');
+                obj.setAttribute('type', 'text/html');
+                if (!window_name && node.title != null) {
+                    obj.setAttribute('data', url_prefix + node.title);
+                }
+                if (window_name) {
+                    var mine = window.open('','','width=1,height=1,left=0,top=0,scrollbars=no');
+                    if(! mine) {
+                         alert(M.str.scorm.popupsblocked);
+                    }
+                    mine.close()
                 }
                 mine.close()
             }
 
-            var old = Y.YUI2.util.Dom.get('scorm_object');
+            var old = Y.one('#scorm_object');
             if (old) {
                 if(window_name) {
                     var cwidth = scormplayerdata.cwidth;
@@ -198,14 +221,6 @@ M.mod_scorm.init = function(Y, hide_nav, hide_toc, toc_title, window_name, launc
                 content.appendChild(obj);
             }
 
-            scorm_resize_frame();
-
-            var left = scorm_layout_widget.getUnitByPosition('left');
-            if (left.expand) {
-                if (!scorm_current_node.state.selected) {
-                    scorm_current_node.select();
-                }
-            }
             if (scorm_hide_nav == false) {
                 scorm_fixnav();
             }
