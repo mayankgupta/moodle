2dd3b9a726290e046ca1a4c7469b64d2d0317770
Mayank Gupta <mayankgupta2005@gmail.com>
2013-07-20 00:10:17 +0530

    MDL-40655 mod_scorm: update scorm_fixnav() to correctly enable/disable nav bttns

--- mod/scorm/module.js
+++ mod/scorm/module.js
@@ -233,11 +233,12 @@ M.mod_scorm.init = function(Y, hide_nav, hide_toc, navposition_type, navposition
          * @return void
          */
         var scorm_fixnav = function() {
-            scorm_buttons[0].set('disabled', ((scorm_skipprev(scorm_current_node) == null) || (scorm_skipprev(scorm_current_node).title == null) ||
-                        (scoes_nav[launch_sco].hideprevious == 1)));
-            scorm_buttons[1].set('disabled', ((scorm_prev(scorm_current_node) === null) || (scorm_prev(scorm_current_node).title == null) || 
-                        (scoes_nav[launch_sco].hideprevious == 1)));
-            scorm_buttons[2].set('disabled', (scorm_up(scorm_current_node) == null) || scorm_up(scorm_current_node).title == null);
+            scorm_buttons[0].set('disabled', ((scorm_skipprev(scorm_current_node) == null) || (scorm_skipprev(scorm_current_node).parent.isRoot()) ||
+                        (scorm_skipprev(scorm_current_node).title == null) || (scoes_nav[launch_sco].hideprevious == 1)));
+            scorm_buttons[1].set('disabled', ((scorm_prev(scorm_current_node) === null) || (scorm_skipprev(scorm_current_node).parent.isRoot()) ||
+                        (scorm_prev(scorm_current_node).title == null) || (scoes_nav[launch_sco].hideprevious == 1)));
+            scorm_buttons[2].set('disabled', (scorm_up(scorm_current_node) == null) || (scorm_up(scorm_current_node).parent.isRoot()) ||
+                         (scorm_up(scorm_current_node).title == null));
             scorm_buttons[3].set('disabled', ((scorm_next(scorm_current_node) == null) || ((scorm_next(scorm_current_node).title == null) &&
                         (scoes_nav[launch_sco].flow != 1)) || (scoes_nav[launch_sco].hidecontinue == 1)));
             scorm_buttons[4].set('disabled', (scorm_skipnext(scorm_current_node) == null || scorm_skipnext(scorm_current_node).title == null ||
